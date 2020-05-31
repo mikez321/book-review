b99a7ecd19aab24c7e642ac08a3c325b9090dc35
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-31 11:40:05 -0600

    Get things passing into the view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,13 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get('http://openlibrary.org/search.json') do |f|
+      f.params['title'] = book_params['title']
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+    @search_result = json[:docs].first
+  end
+
+  def book_params
+    params.permit(:title)
   end
 end
